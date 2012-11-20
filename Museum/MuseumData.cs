3857f550e80fa9f78fd18c395a6eb29368532791
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2012-11-20 23:22:11 +0100

    filling MuseumData.cs

--- Museum/MuseumData.cs
+++ Museum/MuseumData.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Museum
 {
+    // RoomData
+    // Holds info for a single room,, including a list of artworks (Artwork)
     class RoomData
     {
         public string Name { get; set; }
@@ -20,11 +23,22 @@ namespace Museum
         }
     }
 
+    // Artwork
+    // Holds info for a single artwork
     class Artwork
     {
         public string Title { get; set; }
         public string Author { get; set; }
         public string Description { get; set; }
         public Uri Image { get; set; }
+    }
+
+    // MuseumSource
+    // Holds a collection of rooms (RoomData), and contains methods needed to
+    // retreive the rooms.
+    class MuseumSource
+    {
+        private ObservableCollection<RoomData> _Rooms = new ObservableCollection<RoomData>();
+
     }
 }
