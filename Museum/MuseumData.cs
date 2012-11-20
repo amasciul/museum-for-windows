e043ec0d32b9494d2f3b41654d7770372bc6bd58
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2012-11-20 08:58:11 +0100

    first commit

---
+++ Museum/MuseumData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Museum
+{
+    class RoomData
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        private List<Artwork> _artworks = new List<Artwork>();
+        public List<Artwork> Artworks
+        {
+            get
+            {
+                return this._artworks;
+            }
+        }
+    }
+
+    class Artwork
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public Uri Image { get; set; }
+    }
+}
