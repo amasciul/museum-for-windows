7f03092e44b8076b85f358c910933368ddd9d2a0
Alexandre Masciulli <alexandre.masciulli@gmail.com>
2012-11-28 19:05:35 +0100

    now using datacontext more cleverly

--- Museum/GroupedItemsPage.xaml.cs
+++ Museum/GroupedItemsPage.xaml.cs
@@ -18,59 +18,59 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Museum
 {
-    /// <summary>
-    /// Page affichant une collection groupée d'éléments.
-    /// </summary>
-    public sealed partial class GroupedItemsPage : Museum.Common.LayoutAwarePage
-    {
-        public GroupedItemsPage()
-        {
-            this.InitializeComponent();
-        }
+	/// <summary>
+	/// Page affichant une collection groupée d'éléments.
+	/// </summary>
+	public sealed partial class GroupedItemsPage : Museum.Common.LayoutAwarePage
+	{
+		public GroupedItemsPage()
+		{
+			this.InitializeComponent();
+		}
 
-        /// <summary>
-        /// Remplit la page à l'aide du contenu passé lors de la navigation. Tout état enregistré est également
-        /// fourni lorsqu'une page est recréée à partir d'une session antérieure.
-        /// </summary>
-        /// <param name="navigationParameter">Valeur de paramètre passée à
-        /// <see cref="Frame.Navigate(Type, Object)"/> lors de la requête initiale de cette page.
-        /// </param>
-        /// <param name="pageState">Dictionnaire d'état conservé par cette page durant une session
-        /// antérieure. Null lors de la première visite de la page.</param>
-        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
-        {
-            // TODO: créez un modèle de données approprié pour le domaine posant problème pour remplacer les exemples de données
-			MuseumSource museumSource = (MuseumSource)App.Current.Resources["museumSource"];
-			if(museumSource!=null) this.DefaultViewModel["Groups"] = museumSource;
-        }
+		/// <summary>
+		/// Remplit la page à l'aide du contenu passé lors de la navigation. Tout état enregistré est également
+		/// fourni lorsqu'une page est recréée à partir d'une session antérieure.
+		/// </summary>
+		/// <param name="navigationParameter">Valeur de paramètre passée à
+		/// <see cref="Frame.Navigate(Type, Object)"/> lors de la requête initiale de cette page.
+		/// </param>
+		/// <param name="pageState">Dictionnaire d'état conservé par cette page durant une session
+		/// antérieure. Null lors de la première visite de la page.</param>
+		protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+		{
+			// TODO: créez un modèle de données approprié pour le domaine posant problème pour remplacer les exemples de données
+			//MuseumSource museumSource = (MuseumSource)App.Current.Resources["museumSource"];
+			//if(museumSource!=null) this.DefaultViewModel["Groups"] = museumSource;
+		}
 
-        /// <summary>
-        /// Invoqué lorsqu'un utilisateur clique sur un en-tête de groupe.
-        /// </summary>
-        /// <param name="sender">Button utilisé en tant qu'en-tête pour le groupe sélectionné.</param>
-        /// <param name="e">Données d'événement décrivant la façon dont le clic a été initié.</param>
-        void Header_Click(object sender, RoutedEventArgs e)
-        {
-            // Déterminez le groupe représenté par l'instance Button
-            var group = (sender as FrameworkElement).DataContext;
+		/// <summary>
+		/// Invoqué lorsqu'un utilisateur clique sur un en-tête de groupe.
+		/// </summary>
+		/// <param name="sender">Button utilisé en tant qu'en-tête pour le groupe sélectionné.</param>
+		/// <param name="e">Données d'événement décrivant la façon dont le clic a été initié.</param>
+		void Header_Click(object sender, RoutedEventArgs e)
+		{
+			// Déterminez le groupe représenté par l'instance Button
+			var group = (sender as FrameworkElement).DataContext;
 
-            // Accédez à la page de destination souhaitée, puis configurez la nouvelle page
-            // en transmettant les informations requises en tant que paramètre de navigation.
-            this.Frame.Navigate(typeof(GroupDetailPage), ((SampleDataGroup)group).UniqueId);
-        }
+			// Accédez à la page de destination souhaitée, puis configurez la nouvelle page
+			// en transmettant les informations requises en tant que paramètre de navigation.
+			this.Frame.Navigate(typeof(GroupDetailPage), ((SampleDataGroup)group).UniqueId);
+		}
 
-        /// <summary>
-        /// Invoqué lorsqu'un utilisateur clique sur un élément appartenant à un groupe.
-        /// </summary>
-        /// <param name="sender">GridView (ou ListView lorsque l'état d'affichage de l'application est Snapped)
-        /// affichant l'élément sur lequel l'utilisateur a cliqué.</param>
-        /// <param name="e">Données d'événement décrivant l'élément sur lequel l'utilisateur a cliqué.</param>
-        void ItemView_ItemClick(object sender, ItemClickEventArgs e)
-        {
-            // Accédez à la page de destination souhaitée, puis configurez la nouvelle page
-            // en transmettant les informations requises en tant que paramètre de navigation.
-            var itemId = ((SampleDataItem)e.ClickedItem).UniqueId;
-            this.Frame.Navigate(typeof(ItemDetailPage), itemId);
-        }
-    }
+		/// <summary>
+		/// Invoqué lorsqu'un utilisateur clique sur un élément appartenant à un groupe.
+		/// </summary>
+		/// <param name="sender">GridView (ou ListView lorsque l'état d'affichage de l'application est Snapped)
+		/// affichant l'élément sur lequel l'utilisateur a cliqué.</param>
+		/// <param name="e">Données d'événement décrivant l'élément sur lequel l'utilisateur a cliqué.</param>
+		void ItemView_ItemClick(object sender, ItemClickEventArgs e)
+		{
+			// Accédez à la page de destination souhaitée, puis configurez la nouvelle page
+			// en transmettant les informations requises en tant que paramètre de navigation.
+			var itemId = ((SampleDataItem)e.ClickedItem).UniqueId;
+			this.Frame.Navigate(typeof(ItemDetailPage), itemId);
+		}
+	}
 }
